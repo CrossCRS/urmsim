820159dc6dac063d6acf14490b5345952c88f785
Norbert <cross@linux.pl>
2023-03-16 21:46:16 +0100

    Rewrite commands

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::process;
+use std::collections::HashMap;
 use clap::Parser;
 use owo_colors::OwoColorize;
 
+use crate::machine::Machine;
 use crate::parser::UrmParser;
 use crate::prompt::Prompt;
 
@@ -14,13 +16,45 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
-    let args = Cli::parse();
+fn setup_commands(commands: &mut HashMap<&str, fn(&mut Machine, Vec<&str>)>) {
+    commands.insert("exit", |_, _| process::exit(0));
 
+    commands.insert("help", cmd_help);
+    commands.insert("h", cmd_help);
+
+    commands.insert("list", cmd_list);
+    commands.insert("l", cmd_list);
+
+    commands.insert("pc", cmd_pc);
+
+    commands.insert("registers", cmd_registers);
+    commands.insert("reg", cmd_registers);
+
+    commands.insert("run", cmd_run);
+    commands.insert("r", cmd_run);
+
+    commands.insert("step", cmd_step);
+    commands.insert("s", cmd_step);
+}
+
+fn main() {
+    // Setup command HashMap
+    let mut commands: HashMap<&str, fn(&mut Machine, Vec<&str>)> = HashMap::new();
+    setup_commands(&mut commands);
+
+    let args = Cli::parse();
     let mut prompt = Prompt::new();
+
+    // Setup program
     println!("Loading program from {}", args.path.display().bright_white().bold());
 
-    let content = std::fs::read_to_string(&args.path).expect("Couldn't open file");
+    let content = match std::fs::read_to_string(&args.path) {
+        Ok(value) => value,
+        Err(_) => {
+            println!("Couldn't open file {}", args.path.to_str().unwrap_or("").bright_white().bold());
+            process::exit(-1);
+        },
+    };
 
     let parser = UrmParser::new(content);
     let mut machine = parser.parse();
@@ -28,89 +62,102 @@ fn main() {
     println!("Loaded {} instructions", machine.get_instruction_count().bright_white().bold());
     println!();
 
+    // Command loop
     loop {
         if let Ok(cmd) = prompt.read(machine.get_program_counter()) {
-            if cmd == "exit" {
-                process::exit(0);
-            } else if cmd == "run" || cmd == "r" {
-                // TODO: Check if registers are empty and display a warning?
-                println!("Ran {} steps", machine.run().bright_white().bold());
-            } else if cmd == "step" || cmd == "s" {
-                machine.step(true);
-            } else if cmd.starts_with("pc") {
-                let mut args = cmd.split(' ');
-                let arg = args.nth(1);
+            let mut args = cmd.split(' ');
+            let cmd = args.next().unwrap_or("");
+            let args: Vec<&str> = args.collect();
 
-                if let Some(pc) = arg {
-                    let pc = match pc.parse::<usize>() {
-                        Ok(v) => v,
-                        Err(_) => continue,
-                    };
-
-                    match machine.set_program_counter(pc) {
-                        Ok(_) => (),
-                        Err(e) => println!("Couldn't set program counter: {}", e),
-                    }
-                }
-            } else if cmd.starts_with("registers") || cmd.starts_with("reg") {
-                // TODO: Rewrite :(
-                let mut args = cmd.split(' ');
-                let mut arg_ = args.nth(1);
-
-                if let Some(arg) = arg_ {
-                    if arg.contains('=') {
-                        // Set single register to a value
-                        let mut args = arg.split('=');
-                        let reg = match args.next().unwrap().parse::<usize>() {
-                            Ok(v) => v,
-                            Err(_) => continue,
-                        };
-
-                        let value = match args.next().unwrap().parse::<i32>() {
-                            Ok(v) => v,
-                            Err(_) => continue,
-                        };
-
-                        if reg > 0 {
-                            machine.set_register(reg, value);
-                        }
-                    } else {
-                        // Set all registers
-                        let mut i = 1;
-
-                        while let Some(arg) = arg_ {
-                            let value: i32 = match arg.parse::<i32>() {
-                                Ok(v) => v,
-                                Err(_) => break,
-                            };
-
-                            machine.set_register(i, value);
-
-                            i += 1;
-                            arg_ = args.next();
-                        }
-                    }
-                }
-
-                machine.print_registers();
-            } else if cmd == "list" || cmd == "l" {
-                machine.print_instructions();
-            } else if cmd == "help" || cmd == "h" || cmd == "?" {
-                print_help();
+            if commands.contains_key(cmd) {
+                let cmd = commands.get(cmd).unwrap();
+                cmd(&mut machine, args);
+            } else {
+                println!("Unknown command '{}'", cmd.bright_white().bold());
             }
+
             println!();
         }
     }
 }
 
-fn print_help() {
+/// Print all available commands
+fn cmd_help(_machine: &mut Machine, _args: Vec<&str>) {
     println!("{}\t\t\tShow this help screen", "(h)elp".bright_white().bold());
     println!("{}\t\t\tShow program instructions", "(l)ist".bright_white().bold());
-    println!("{}\t\t\tShow program counter value", "pc".bright_white().bold());
     println!("{} value\t\tSet program counter value", "pc".bright_white().bold());
     println!("{}\t\tShow registers content", "(reg)isters".bright_white().bold());
     println!("{} x y\t\tSet registers content (R1 = 0, R2 = y...)", "(reg)isters".bright_white().bold());
     println!("{} x=y\t\tSet registers content (Rx = y)", "(reg)isters".bright_white().bold());
     println!("{}\t\t\tRun the whole program", "(r)un".bright_white().bold());
     println!("{}\t\t\tRun a single instruction", "(s)tep".bright_white().bold());
+}
+
+/// List currently loaded instructions
+fn cmd_list(machine: &mut Machine, _args: Vec<&str>) {
+    machine.print_instructions();
+}
+
+/// Set program counter
+fn cmd_pc(machine: &mut Machine, args: Vec<&str>) {
+    let arg = args.first();
+
+    if let Some(pc) = arg {
+        let pc = match pc.parse::<usize>() {
+            Ok(v) => v,
+            Err(_) => return,
+        };
+
+        match machine.set_program_counter(pc) {
+            Ok(_) => (),
+            Err(e) => println!("Couldn't set program counter: {}", e),
+        }
+    }
+}
+
+/// Display or set registers
+fn cmd_registers(machine: &mut Machine, args: Vec<&str>) {
+    let mut i = 1;
+
+    for arg in &args {
+        if arg.contains('=') {
+            // Set a specified register
+            let mut reg_args = arg.split('=');
+            let reg = match reg_args.next().unwrap().parse::<usize>() {
+                Ok(v) => v,
+                Err(_) => return,
+            };
+
+            let value = match reg_args.next().unwrap().parse::<i32>() {
+                Ok(v) => v,
+                Err(_) => return,
+            };
+
+            if reg > 0 {
+                machine.set_register(reg, value);
+            }
+        } else {
+            // Set a register based on argument index
+            let value: i32 = match arg.parse::<i32>() {
+                Ok(v) => v,
+                Err(_) => break,
+            };
+
+            machine.set_register(i, value);
+        }
+        i += 1;
+    }
+
+    machine.print_registers();
+}
+
+/// Run all remaining instructions
+fn cmd_run(machine: &mut Machine, _args: Vec<&str>) {
+    // TODO: Check if registers are empty and display a warning?
+    println!("Ran {} steps", machine.run().bright_white().bold());
+}
+
+/// Run a single instruction
+fn cmd_step(machine: &mut Machine, _args: Vec<&str>) {
+    machine.step(true);
 }
